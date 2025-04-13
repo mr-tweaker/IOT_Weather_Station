89a436c1ea00998bf91a7c2f9d0dcf07ddf07507
mr-tweaker <aniket.lamba@gmail.com>
2025-04-13 10:30:54 +0530

    Directly pick the API key before the AWS Amplify build

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
-// Firebase configuration - using environment variables
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+// config.js
+// Use the global firebase config
+const firebaseConfig = window.firebaseConfig || {};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
---
+++ env-config.js
@@ -0,0 +1,11 @@
+// env-config.js
+window.firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "iotbasedws.firebaseapp.com",
+    databaseURL: "https://iotbasedws-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "iotbasedws",
+    storageBucket: "iotbasedws.firebasestorage.app",
+    messagingSenderId: "623007834469",
+    appId: "1:623007834469:web:1b02788b7f41bb540ec18c",
+    measurementId: "G-EGNYJJNS7X"
+  };
